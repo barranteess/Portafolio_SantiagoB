19f6cdeb5411da24d5bb3285626542d8b724f3a7
santiago barrantes <barrantessantiago0@gmail.com>
2026-09-16 20:44:48 -0600

    semana 2

---
+++ Portafolio_SantiagoB/Portafolio_SantiagoB/src/test/java/Portafolio_SantiagoB/Portafolio/PortafolioApplicationTests.java
@@ -0,0 +1,13 @@
+package Portafolio_SantiagoB.Portafolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
